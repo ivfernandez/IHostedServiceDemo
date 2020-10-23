25d63c82a114a45ed2e2be98088a180b2a559b78
Ivan <fernandezivanjavier@gmail.com>
2020-10-23 15:53:35 -0300

    Agregar archivos de proyecto.

---
+++ Services/WriteToFileHostedService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IHostedServiceDemo.Services
+{
+    public class WriteToFileHostedService : IHostedService, IDisposable
+    {
+        private readonly IHostEnvironment environment;
+        private readonly string fileName = "File 1.txt";
+        private Timer timer;
+
+        public WriteToFileHostedService(IHostEnvironment environment)
+        {
+            this.environment = environment;
+
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService: Process Started");
+
+            //Configuro el timer para que se ejecute DoWork cada 5 segundos.
+            timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            WriteToFile("WriteToFileHostedService: Doing some work at " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService: Process Stopped");
+            timer?.Change(Timeout.Infinite, 0); //Desactivo el timer.
+            return Task.CompletedTask;
+        }
+
+        private void WriteToFile(string message)
+        {
+            var path = $@"{environment.ContentRootPath}\wwwroot\{fileName}";
+            using (StreamWriter writer = new StreamWriter(path, append: true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+}
